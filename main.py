9a60357918cecaf9bdb42ccdf3cd5af16774ba36
OA203044 <76588271+OA203044@users.noreply.github.com>
2021-03-23 18:09:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from config import WeLogin, SendMail, wakeDyno
+import schedule
+import time
 
 
-WeLogin()
-SendMail()
+schedule.every(28).minutes.do(wakeDyno)
+
+def job():
+  WeLogin()
+  SendMail()
+  
+schedule.every(2).day.at("4:25").do(job)
+
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
 
   
  
